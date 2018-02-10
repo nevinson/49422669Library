64622b3ab311078d86fffdded328ee97d4e7c9ed
NEVINSON\Nevinson <nevinson@outlook.com>
2018-02-10 09:42:50 +0200

    book reserve service created....

---
+++ 49422669Library/BusinessLogic/ReserveService.vb
@@ -0,0 +1,180 @@
+﻿Imports System.Data.OleDb
+
+Public Class ReserveService
+    ''
+    Dim sql As String = Nothing
+    Dim blResult As Boolean = False
+    Dim objConstants As New Constants()
+
+    Public Function GetAll() As DataTable
+        ''
+        Dim dbCon As New OleDbConnection()
+        Dim dbCmd As New OleDbCommand()
+        Dim dbTable As New DataTable()
+        sql = "GetAllReserves"
+        blResult = False
+
+        ''
+        dbCon.ConnectionString = objConstants.ConnectionString()
+        dbCon.Open()
+
+        ''
+        dbCmd.Connection = dbCon
+        dbCmd.CommandText = sql
+        dbCmd.CommandType = CommandType.StoredProcedure
+
+        ''
+        dbTable.Load(dbCmd.ExecuteReader())
+
+        ''
+        Return dbTable
+    End Function
+
+    Public Function GetByBookNumber(ByVal strBookNumber As String) As DataTable
+        ''
+        Dim dbCon As New OleDbConnection()
+        Dim dbCmd As New OleDbCommand()
+        Dim dbTable As New DataTable()
+        sql = "GetReserveByBookNumber"
+        blResult = False
+
+        ''
+        dbCon.ConnectionString = objConstants.ConnectionString()
+        dbCon.Open()
+
+        ''
+        dbCmd.Connection = dbCon
+        dbCmd.CommandText = sql
+        dbCmd.CommandType = CommandType.StoredProcedure
+
+        ''
+        dbCmd.Parameters.AddWithValue("@bookNumber", strBookNumber)
+
+        ''
+        dbTable.Load(dbCmd.ExecuteReader())
+
+        ''
+        Return dbTable
+    End Function
+
+    Public Function GetByMembershipNumber(ByVal strMembershipNumber As String) As DataTable
+        ''
+        Dim dbCon As New OleDbConnection()
+        Dim dbCmd As New OleDbCommand()
+        Dim dbTable As New DataTable()
+        sql = "GetReserveByMembershipNumber"
+        blResult = False
+
+        ''
+        dbCon.ConnectionString = objConstants.ConnectionString()
+        dbCon.Open()
+
+        ''
+        dbCmd.Connection = dbCon
+        dbCmd.CommandText = sql
+        dbCmd.CommandType = CommandType.StoredProcedure
+
+        ''
+        dbCmd.Parameters.AddWithValue("@membershipNumber", strMembershipNumber)
+
+        ''
+        dbTable.Load(dbCmd.ExecuteReader())
+
+        ''
+        Return dbTable
+    End Function
+
+    Public Function Create(ByVal _bookReserve As BookReserve) As Boolean
+        ''
+        Dim dbCon As New OleDbConnection()
+        Dim dbCmd As New OleDbCommand()
+        sql = "CreateBookReserve"
+        blResult = False
+
+        ''
+        dbCon.ConnectionString = objConstants.ConnectionString()
+        dbCon.Open()
+
+        ''
+        dbCmd.Connection = dbCon
+        dbCmd.CommandText = sql
+        dbCmd.CommandType = CommandType.StoredProcedure
+
+        ''
+        dbCmd.Parameters.AddWithValue("@reserveDate", _bookReserve.ReserveDate)
+        dbCmd.Parameters.AddWithValue("@membershipNumber", _bookReserve.MembershipNumber)
+        dbCmd.Parameters.AddWithValue("@bookNumber", _bookReserve.BookNumber)
+        dbCmd.Parameters.AddWithValue("@reserveId", _bookReserve.ReserveId)
+
+        Try
+            If dbCmd.ExecuteNonQuery() = 1 Then
+                blResult = True
+            Else
+                blResult = False
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString(), "Create User", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+        Return blResult
+    End Function
+
+    Public Function Update(ByVal _bookReserve As BookReserve)
+        ''
+        Dim dbCon As New OleDbConnection()
+        Dim dbCmd As New OleDbCommand()
+        sql = "UpdateBookReserve"
+        blResult = False
+
+        ''
+        dbCon.ConnectionString = objConstants.ConnectionString()
+        dbCon.Open()
+
+        ''
+        dbCmd.Connection = dbCon
+        dbCmd.CommandText = sql
+        dbCmd.CommandType = CommandType.StoredProcedure
+
+        ''
+        dbCmd.Parameters.AddWithValue("@reserveDate", _bookReserve.ReserveDate)
+        dbCmd.Parameters.AddWithValue("@membershipNumber", _bookReserve.MembershipNumber)
+        dbCmd.Parameters.AddWithValue("@bookNumber", _bookReserve.BookNumber)
+        dbCmd.Parameters.AddWithValue("@reserveId", _bookReserve.ReserveId)
+
+        If dbCmd.ExecuteNonQuery() = 1 Then
+            blResult = True
+        Else
+            blResult = False
+        End If
+
+        Return blResult
+    End Function
+
+    Public Function Delete(ByVal intReserveId As Integer)
+        ''
+        Dim dbCon As New OleDbConnection()
+        Dim dbCmd As New OleDbCommand()
+        sql = "DeleteBookReserve"
+        blResult = False
+
+        ''
+        dbCon.ConnectionString = objConstants.ConnectionString()
+        dbCon.Open()
+
+        ''
+        dbCmd.Connection = dbCon
+        dbCmd.CommandText = sql
+        dbCmd.CommandType = CommandType.StoredProcedure
+
+        ''
+        dbCmd.Parameters.AddWithValue("@reserveId", intReserveId)
+
+        If dbCmd.ExecuteNonQuery() = 1 Then
+            blResult = True
+        Else
+            blResult = False
+        End If
+
+        Return blResult
+    End Function
+End Class
